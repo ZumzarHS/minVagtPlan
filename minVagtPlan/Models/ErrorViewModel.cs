a54ae83a68e5e13a39578338ad75b0830356a37a
Rasmus Hougaard Schjelder <94530862+ZumzarHS@users.noreply.github.com>
2024-11-29 21:57:19 +0100

    Revert "Updated readme and used .net 9.0"

--- minVagtPlan/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace minVagtPlan.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
